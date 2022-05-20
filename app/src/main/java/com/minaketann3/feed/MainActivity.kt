225a3fd1b37b1814f5907f988b597dcc7a457e08
MK Nayak <84683862+mkgit01@users.noreply.github.com>
2022-05-20 20:13:50 +0530

    first commit

---
+++ app/src/main/java/com/minaketann3/feed/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.minaketann3.feed
+
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import org.json.JSONObject
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+
+
+
+
+class MainActivity : AppCompatActivity(),NewsItemClicked {
+
+    private lateinit var mAdapter:NewsListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
+    }
+
+    private fun fetchData(){
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener {
+            val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage"),
+                        newsJsonObject.getString("publishedAt")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+            },
+            Response.ErrorListener{
+
+            })
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    override fun onItemClicked(item:News){
+        val builder = CustomTabsIntent.Builder();
+        val customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(item.url));
+    }
+}
---
+++ app/src/main/java/com/minaketann3/feed/News.kt
@@ -0,0 +1,9 @@
+package com.minaketann3.feed
+
+data class News(
+    val title:String,
+    val author:String,
+    val url:String,
+    val imageUrl:String,
+    val published:String
+)
